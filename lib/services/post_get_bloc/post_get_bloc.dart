6cdd00276162242ed4861a1cd63ae78b1a13c72e
David <zdavid1@myges.fr>
2023-11-11 16:55:43 +0100

    get post WIP

---
+++ lib/services/post_get_bloc/post_get_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'post_get_event.dart';
+part 'post_get_state.dart';
+
+class PostGetBloc extends Bloc<PostGetEvent, PostGetState> {
+  PostGetBloc() : super(PostGetInitial()) {
+    on<PostGetEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
