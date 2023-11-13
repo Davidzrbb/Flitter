cd694e65e61eed6c211aee2a196b942c38a97ad3
David <zdavid1@myges.fr>
2023-11-13 18:28:25 +0100

    delete

--- lib/utils/delete_post_icon.dart
+++ lib/utils/delete_post_icon.dart
@@ -3,16 +3,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../services/post_delete_bloc/post_delete_bloc.dart';
 import '../services/post_get_bloc/post_get_bloc.dart';
 
-class DeletePostIcon extends StatefulWidget {
+class DeletePostIcon extends StatelessWidget {
   const DeletePostIcon({Key? key, required this.id}) : super(key: key);
 
   final int id;
 
-  @override
-  State<DeletePostIcon> createState() => _DeletePostIconState();
-}
-
-class _DeletePostIconState extends State<DeletePostIcon> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -25,27 +20,43 @@ class _DeletePostIconState extends State<DeletePostIcon> {
     );
   }
 
-  _onDeleted(BuildContext context) async {
-    if (!mounted) {
-      // The widget is no longer in the widget tree, so don't proceed
-      return;
-    }
+  Future<void> _onDeleted(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext dialogContext) {
+        return BlocListener<PostDeleteBloc, PostDeleteState>(
+          listener: (context, state) {
+            if (state.status == PostDeleteStatus.success) {
+              BlocProvider.of<PostGetBloc>(context)
+                  .add(PostGetAll(refresh: true));
+              Navigator.of(context).pop();
+            }
+          },
+          child: AlertDialog(
+            title: const Text("Confirmation"),
+            content: const Text("Êtes-vous sûr de vouloir supprimer ce post ?"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Annuler"),
+              ),
+              TextButton(
+                onPressed: () {
+                  _onDeletedBloc(context);
+                },
+                child: const Text("Supprimer",
+                    style: TextStyle(color: Colors.red)),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
-    BlocProvider.of<PostDeleteBloc>(context).add(PostDelete(widget.id));
-    BlocProvider.of<PostDeleteBloc>(context).stream.listen((state) {
-      if (!mounted) {
-        return;
-      }
-
-      if (state.status == PostDeleteStatus.success) {
-        BlocProvider.of<PostGetBloc>(context).add(PostGetAll(refresh: true));
-         ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Post supprimé'),
-          duration: Duration(seconds: 3),
-        ),
-      );
-      }
-    });
+  void _onDeletedBloc(BuildContext context) {
+    BlocProvider.of<PostDeleteBloc>(context).add(PostDelete(id));
   }
 }
