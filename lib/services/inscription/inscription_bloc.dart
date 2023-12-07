ad6bd2ec7c6b9394d6c953583fb5a8f9d702a0ab
David <zdavid1@myges.fr>
2023-12-07 17:45:31 +0100

    inscription repo

--- lib/services/inscription/inscription_bloc.dart
+++ lib/services/inscription/inscription_bloc.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
